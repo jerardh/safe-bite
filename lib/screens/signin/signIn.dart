c0ecb3e745c4ee2c3dcb4ed407ec0f065034824c
jerardh <jerardhtomj@gmail.com>
2025-09-22 14:36:11 +0530

    ADD Hashing in UserData

--- lib/screens/signin/signIn.dart
+++ lib/screens/signin/signIn.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:safebite/screens/home/home.dart';
 import 'package:safebite/util/AppText.dart';
+import 'package:safebite/util/HashingHelper.dart';
 import 'package:safebite/util/appColor.dart';
 
 class SignIn extends StatefulWidget {
@@ -15,22 +17,41 @@ class SignIn extends StatefulWidget {
 }
 
 class SignInState extends State<SignIn> {
+  final CollectionReference userInfo =
+      FirebaseFirestore.instance.collection('userInfo');
   final TextEditingController _userNameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  Hashinghelper hashinghelper = Hashinghelper();
   Future<void> login() async {
     String userName = _userNameController.text.trim();
-    String password = _passwordController.text.trim();
+    String password = hashinghelper.hashString(_passwordController.text.trim());
     if (userName.isEmpty || password.isEmpty) {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text("Please enter username and password")),
       );
       return;
+    } else {
+      // print("Hashed password=" + password);
+      QuerySnapshot querySnapshot = await userInfo.get();
+      var flag = false;
+      for (var doc in querySnapshot.docs) {
+        Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+        var storeduserId = data['userName'];
+        var storedpassword = data['passWord'];
+        if (storeduserId == userName && password == storedpassword) {
+          flag = true;
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => Home()),
+          );
+          break;
+        }
+      }
+      if (!flag) {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+            content: Text("Please enter a valid username and password")));
+      }
     }
-
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(builder: (context) => Home()),
-    );
   }
 
   @override
