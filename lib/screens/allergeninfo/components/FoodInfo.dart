19fe6ee2ca720e6a9abc5ea9d63a9ef73aaec720
jerardh <jerardhtomj@gmail.com>
2025-10-04 17:36:11 +0530

    UPDATE FoodInfo

--- lib/screens/allergeninfo/components/FoodInfo.dart
+++ lib/screens/allergeninfo/components/FoodInfo.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:safebite/util/AppText.dart';
+import 'package:safebite/util/appColor.dart';
 import 'package:safebite/util/util.dart';
 
 class FoodInfo extends StatelessWidget {
@@ -15,51 +16,83 @@ class FoodInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: Util().appBar,
-        body: Container(
-            width: 600,
-            child: Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5),
-              ),
-              elevation: 3,
-              margin: const EdgeInsets.all(10),
-              child: Padding(
-                padding: const EdgeInsets.all(5.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    // Food Name
-                    Text(foodName, style: AppText().secondaryStyle),
-                    const SizedBox(height: 10),
-
-                    // Ingredients Title
-                    Text(
-                      "Ingredients:",
-                      style: AppText().secondaryStyle,
+      appBar: Util().appBar,
+      body: Container(
+        decoration: BoxDecoration(),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Dish Card
+                Card(
+                  elevation: 6,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  color: AppColor.primary,
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 30,
+                          backgroundColor: Colors.white,
+                          child: Icon(Icons.fastfood,
+                              color: AppColor.primary, size: 30),
+                        ),
+                        SizedBox(width: 16),
+                        Text(
+                          foodName,
+                          style: AppText().primaryStyle,
+                        ),
+                      ],
                     ),
-                    const SizedBox(height: 8),
-
-                    // Ingredient List
-                    ...ingredients.map(
-                      (item) => Row(
-                        children: [
-                          const Icon(Icons.food_bank,
-                              size: 18, color: Colors.green),
-                          const SizedBox(width: 8),
-                          Expanded(
-                            child: Text(
-                              item,
-                              style: AppText().analysistextStyle,
-                            ),
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
+                  ),
                 ),
-              ),
-            )));
+                SizedBox(height: 20),
+
+                Text(
+                  "Ingredients",
+                  style: AppText().secondaryStyle,
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 12),
+
+                // Ingredients as stylish cards
+                Expanded(
+                  child: GridView.builder(
+                    itemCount: ingredients.length,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 3,
+                      crossAxisSpacing: 12,
+                      mainAxisSpacing: 12,
+                    ),
+                    itemBuilder: (context, index) {
+                      return Card(
+                        elevation: 4,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        color: Colors.white,
+                        child: Center(
+                          child: Text(
+                            ingredients[index],
+                            style: AppText().hintTextStyle,
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
